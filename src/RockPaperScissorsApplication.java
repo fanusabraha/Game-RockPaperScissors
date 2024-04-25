3561e7f45c37673a60020c8cbe0f79f7bdeef6b5
fanus abraha <fanus.abraha@yahoo.com>
2024-04-25 09:51:03 -0500

    Creating a Game

--- src/RockPaperScissorsApplication.java
+++ src/RockPaperScissorsApplication.java
@@ -1,3 +1,9 @@
+import java.util.Scanner;
+
 public class RockPaperScissorsApplication {
-    
+    public static void main(String[] args) {
+        Scanner scnanner= new Scanner(System.in);
+        String [] game= {"r","p","s"};
+        String playermove;
+        }
 }
