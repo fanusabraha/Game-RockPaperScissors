f08c4ebe5c5850aed86836ce5ad96632f8044373
fanus abraha <fanus.abraha@yahoo.com>
2024-04-25 09:52:41 -0500

    Creating a while loop

--- src/RockPaperScissorsApplication.java
+++ src/RockPaperScissorsApplication.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class RockPaperScissorsApplication {
@@ -5,5 +6,13 @@ public class RockPaperScissorsApplication {
         Scanner scnanner= new Scanner(System.in);
         String [] game= {"r","p","s"};
         String playermove;
+        while (true) {
+            String computermove = game[new Random().nextInt(game.length)];
+            while (true) {
+                System.out.println("ENter r for rock s for scissors p paper");
+                playermove = scnanner.next();
+                if (playermove.equals("r") || playermove.equals("p") || playermove.equals("s")) ;
+                break;
+            }
         }
 }
