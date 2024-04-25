01981a1e1d34eac9f1fe00356134e5c323449eb3
fanus abraha <fanus.abraha@yahoo.com>
2024-04-25 09:54:02 -0500

    Creating a if loop

--- src/RockPaperScissorsApplication.java
+++ src/RockPaperScissorsApplication.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 
 public class RockPaperScissorsApplication {
     public static void main(String[] args) {
-        Scanner scnanner= new Scanner(System.in);
-        String [] game= {"r","p","s"};
+        Scanner scnanner = new Scanner(System.in);
+        String[] game = {"r", "p", "s"};
         String playermove;
         while (true) {
             String computermove = game[new Random().nextInt(game.length)];
@@ -14,5 +14,9 @@ public class RockPaperScissorsApplication {
                 if (playermove.equals("r") || playermove.equals("p") || playermove.equals("s")) ;
                 break;
             }
+            if (playermove.equals(computermove)) {
+                System.out.println(" NOdbody won your move was  " + playermove + " Computermove was " + computermove);
+            }
         }
+    }
 }
